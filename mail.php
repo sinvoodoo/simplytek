25f1375635ad874e2109d597dc26f0662ecd0b57
sinvoodoo <48058963+sinvoodoo@users.noreply.github.com>
2021-02-14 14:12:37 +0000

    Add files via upload

--- mail.php
+++ mail.php
@@ -1,12 +1,24 @@
 <?php
-if(isset( $_POST['Name']))
-$name = $_POST['Name'];
+if(isset( $_POST['name']))
+$name = $_POST['name'];
 
-if(isset( $_POST['Email']))
-$email = $_POST['Email'];
+if(isset( $_POST['email']))
+$email = $_POST['email'];
+
+if(isset( $_POST['message']))
+$email = $_POST['message'];
+
+if(isset( $_POST['newsletter']))
+$email = $_POST['newsletter'];
+
+if(isset( $_POST['contact-pref']))
+$email = $_POST['contact-pref'];
+
+
 
 $content="From: $name";
 $recipient = "simplytekblog@gmail.com";
+$subject = "My subject";
 $mailheader = "From: $email \r\n";
 mail($recipient, $subject, $content, $mailheader) or die("Error!");
 echo "Email sent!";
